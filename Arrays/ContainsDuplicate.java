82adbeacea00b4abc6625cd52dcda17481d018f5
gurpreetsingh <gurpreet24277@gmail.com>
2023-02-09 03:33:59 -0500

    IsAnagram

--- Arrays/ContainsDuplicate.java
+++ Arrays/ContainsDuplicate.java
@@ -7,11 +7,11 @@ public class ContainsDuplicate {
     public static void main (String args[]) {
         int[] nums = {2,2,4};
 
-        boolean result = TwoSumWrapper(nums);
+        boolean result = ContainsDuplicateWrapper(nums);
         System.out.println(result);
     }
 
-    public static boolean TwoSumWrapper(int nums[]) {
+    public static boolean ContainsDuplicateWrapper(int nums[]) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(nums[i])) {
