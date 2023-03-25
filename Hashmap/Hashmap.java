3fd491ea3e8721d1aaad2fa003a2ab595f390a4d
gurpreetsingh <gurpreet24277@gmail.com>
2023-03-25 19:34:23 -0400

    Hashmap Implementation

---
+++ Hashmap/Hashmap.java
@@ -0,0 +1,16 @@
+package Hashmap;
+
+// Using Separate Chaining Technique for avoiding hash collision
+class HashMap {
+    private int arr;
+
+    public void add(int value) {
+
+    }
+}
+
+public class Hashmap {
+    public static void main(String args[]) {
+
+    }
+}
